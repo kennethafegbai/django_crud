ad7e569cdd65b2bd8a483b796f9976e673db952e
Kenneth Afegbai <kennethafegbai@gmail.com>
2022-06-25 23:22:21 +0100

    add .gitignore

--- I4G002265AHN/blog/views.py
+++ I4G002265AHN/blog/views.py
@@ -1,5 +1,10 @@
-#from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.shortcuts import render
+from django.urls import reverse_lazy
+#from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 from .models import Post
 
 
@@ -9,25 +14,25 @@ class PostListView(ListView):
 
 class  PostCreateView(CreateView):
     model = Post
-    fields = __all__
-    success_url  = reverse_lazy(blog:all)
+    fields = "__all__"
+    success_url  = reverse_lazy('blog:all')
+
 
 class PostDetailView(DetailView):
     model = Post
 
+
 class PostUpdateView(UpdateView):
     model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy('blog:all')
 
-    fields = __all__
-
-    success_url  = reverse_lazy(blog:all)
 
 class PostDeleteView(DeleteView):
     model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy('blog:all')
 
-    fields = __all__
-
-    success_url  = reverse_lazy(blog:all)
 
 
 
